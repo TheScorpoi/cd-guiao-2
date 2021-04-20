c319933768b303615d41559fc9e1a41c96adca60
Diogo Gomes <dgomes@ua.pt>
2021-04-20 15:27:11 +0100

    arg.parser

--- DHT.py
+++ DHT.py
@@ -1,10 +1,11 @@
 import logging
 import time
 import sys
+import argparse
 from DHTNode import DHTNode
 
 
-def main(number_nodes):
+def main(number_nodes, timeout):
     """ Script to launch several DHT nodes. """
 
     # logger for the main
@@ -20,7 +21,7 @@ def main(number_nodes):
     for i in range(number_nodes - 1):
         time.sleep(0.2)
         # Create DHT_Node threads on ports 5001++ and with initial DHT_Node on port 5000
-        node = DHTNode(("localhost", 5001 + i), ("localhost", 5000))
+        node = DHTNode(("localhost", 5001 + i), ("localhost", 5000), timeout)
         node.start()
         dht.append(node)
         logger.info(node)
@@ -36,20 +37,22 @@ def main(number_nodes):
 if __name__ == "__main__":
     # Launch DHT with 5 Nodes
 
-    if "savelog" in sys.argv:
-        # configure the log with DEBUG level
-        logging.basicConfig(
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--savelog", default=False, action="store_true")
+    parser.add_argument("--nodes", type=int, default=5)
+    parser.add_argument("--timeout", type=int, default=3)
+    args = parser.parse_args()
+
+    logfile = {}
+    if args.savelog:
+        logfile = {"filename":"dht.txt", "filemode": "w"}
+
+    logging.basicConfig(
             level=logging.DEBUG,
             format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
             datefmt="%m-%d %H:%M:%S",
-            filename="dht.txt",
-            filemode="w"
-        )
-    else:
-        logging.basicConfig(
-            level=logging.DEBUG,
-            format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-            datefmt="%m-%d %H:%M:%S",
+            **logfile
         )
 
-    main(5)
+
+    main(args.nodes, timeout=args.timeout)
