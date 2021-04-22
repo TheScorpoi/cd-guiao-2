3323503700ed5fc16e8c27e77999e22aabbf81a5
Diogo Gomes <dgomes@ua.pt>
2021-04-22 22:31:20 +0100

    test_finger_table_used

--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -3,6 +3,7 @@ import pytest
 import time
 from DHTClient import DHTClient
 from DHTNode import DHTNode
+from unittest.mock import patch, MagicMock
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -83,3 +84,23 @@ def test_actual_node_finger_table(node1, node2):
         (257, ("localhost", 5003)),
         (257, ("localhost", 5003)),
     ]
+
+def test_finger_table_used(client, node1, node2):
+    assert node1.identification == 895
+    assert node1.successor_id == 959
+
+
+    assert node2.identification == 752
+    assert node2.successor_id == 770
+
+    with patch.object(node1, "put", MagicMock(side_effect=node1.put)) as put1:
+        with patch.object(node2, "put", MagicMock(side_effect=node2.put)) as put2:
+
+            assert client.put("d", "That tickles")  # dht_hash("d") = 115
+
+            assert put1.call_count == 0
+            assert put2.call_count == 0
+
+            assert client.put("f", "No sweat")  # dht_hash("f") = 921
+            assert put1.call_count == 1
+            assert put2.call_count == 0
